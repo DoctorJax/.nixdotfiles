e38c63c69094f58782aa70a85f1a0d567d24191a
DoctorJax <jdl2016nc@gmail.com>
2023-05-31 01:06:51 -0400

    things are slowly (very slowly, like a fuckin' snail) coming together

--- hypr/default.nix
+++ hypr/default.nix
@@ -7,33 +7,8 @@
   ];
 
   home.file = {
-    ".config/hypr/autostart.sh".text = ''
-      # Fancy Stuff
-      /home/jackson/.config/eww/launch_bar &
-      /home/jackson/.scripts/wallpapers.sh -i &
-      swaync &
-
-      # Useful Stuff
-      /usr/bin/emacs --daemon &
-      headsetcontrol -l 0 &
-
-      # Steam was being a pain
-      xrandr --output HDMI-A-1 --primary &
-
-      # MPD Stuff
-      pgrep -x mpd > /dev/null || mpd /home/jackson/.config/mpd/mpd.conf &
-      pgrep -x mpd-mpris > /dev/null || mpd-mpris &
-
-      # Run Once
-      pgrep -x companion.sh > /dev/null || /home/jackson/.scripts/companion.sh &
-
-      pgrep -x thunderbird > /dev/null || thunderbird &
-      pgrep -x signal-desktop > /dev/null || signal-desktop --ozone-platform=wayland &
-      pgrep -x DiscordCanary > /dev/null || discord-canary --enable-features=UseOzonePlatform --ozone-platform=wayland &
-
-      # Polkit
-      /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval "$(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)" &
-    '';
+    ".config/hypr/autostart.sh".source = ./autostart.sh;
+    ".config/hypr/portal-wlr.sh".source = ./portal-wlr.sh;
   };
 
   wayland.windowManager.hyprland.extraConfig = ''
@@ -63,7 +38,6 @@
     # -------------------------
     exec=$HOME/.config/hypr/autostart.sh
     exec=$HOME/.config/hypr/portal-wlr.sh
-    exec-once=$HOME/.local/share/hyprload/hyprload.sh
 
     # -------------------------
     # Hyprland Config
